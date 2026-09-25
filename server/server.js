deef3bcac5daef398b9705560bbf2e317d9f1c22
Nikhil <xharmanikk9876@gmail.com>
2026-09-25 22:42:44 +0530

    Fix Gmail IPv4 connection

--- server/server.js
+++ server/server.js
@@ -43,12 +43,16 @@ const groq = new Groq({
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+
+    family: 4,
 });
 
 app.use(cors());
